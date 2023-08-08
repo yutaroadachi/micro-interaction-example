8f019851e04d1d8745f811aaa89b29f6d91718f5
yutaroadachi <48372923+yutaroadachi@users.noreply.github.com>
2023-08-08 23:09:34 +0900

    feat: add the LikeButton component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
   <main>
     <h1>Micro Interaction Example</h1>
+    <section>
+      <LikeButton />
+    </section>
   </main>
 </template>
+
+<script setup>
+import LikeButton from "@/components/LikeButton";
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: black;
+  color: white;
+}
+</style>
+
+<style scoped>
+main {
+  padding: 0 16px;
+}
+</style>
---
+++ src/components/LikeButton/PolkaDots.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="PolkaDots"></span>
+</template>
+
+<style scoped>
+.PolkaDots {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 36px;
+  height: 36px;
+  background-color: transparent;
+  border: 2px dotted #f91880;
+  border-radius: 50%;
+  opacity: 0%;
+  animation-name: scale-and-fade;
+  animation-duration: 1s;
+}
+
+@keyframes scale-and-fade {
+  0% {
+    opacity: 100%;
+  }
+  100% {
+    transform: scale(150%);
+  }
+}
+</style>
